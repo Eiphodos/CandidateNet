a999c556c162fd121b76826850a31f3ad26a6149
Eiphodos <david.hagerman.olzon@gmail.com>
2024-12-03 15:37:46 +0100

    Woopsie

--- training_metaloss.py
+++ training_metaloss.py
@@ -120,7 +120,7 @@ def main(args):
             ce_loss = ce_loss_criterion(outputs, labels.squeeze(1).long())
             dice_ce_loss = dice_loss + ce_loss.mean()
             loss = dice_ce_loss
-            if len(meta_losses > 0):
+            if len(meta_losses) > 0:
                 meta_loss = compute_meta_loss(ce_loss, meta_losses, meta_losses_coords, meta_loss_criterion,
                                               args.patch_sizes)
                 meta_loss = meta_loss * args.meta_beta
